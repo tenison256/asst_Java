a4bfd5a91733c6d6f19e6b1e2ec89f058cb5e5d5
tenison256 <65032061+tenison256@users.noreply.github.com>
2020-06-14 19:07:29 +0500

    Add files via upload

---
+++ Chap 2/Chap 3/Part 1-3/app.js
@@ -0,0 +1,9 @@
+//part 1//
+var age=23;
+alert("I am "+age+" years old");
+//part 2//
+var a =0;
+alert("You have visited the web " + a++ + "time");
+//part 3//
+var birthYear=1998;
+document.write("My birth year is "+ birthYear + "<br> Declared datatype of my variable is a number.");
---
+++ Chap 2/Chap 3/Part 4/app.js
@@ -0,0 +1,5 @@
+//part 4//
+var a = prompt("Welcome to online store www"+"." +"xyz"+"Clothing"+"."+"com \n Enter visitor's name","Visitor's name is");
+var b = prompt("Welcome to online store www"+"." +"xyz"+"Clothing"+"."+"com \n Enter Product title"," Product title is ");
+var c = prompt("Welcome to online store www"+"." +"xyz"+"Clothing"+"."+"com \n Enter Quantity"," Quantity is ");
+document.write("\n"+"<b>"+a+"</b>"+ " ordered "+"<b>"+c+"</b>"+ " <b>"+b+"</b>"+"(s) "+" on " + "XYZ "+"Clothing "+"Store");
