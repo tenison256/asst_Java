239869778dfad2107163ecfd92ecf6850e0d9446
tenison256 <65032061+tenison256@users.noreply.github.com>
2020-06-14 19:06:45 +0500

    Add files via upload

---
+++ Chap 2/app.js
@@ -0,0 +1,28 @@
+//part 1//
+var username;
+// part 2//
+var myName= "Syeda Arzu Fatima";
+//part 3//
+var message= "Hello World";
+alert(message);
+//part 4//
+var stdName="Jhone Doe";
+alert(stdName);
+var age= "15 years old";
+alert(age);
+var course="Certified Mobile Application Development";
+alert(course);
+//part 5//
+var a="PIZZA";
+alert(a+"\n PIZZ \n PIZ \n PI \n P");
+//part 6//
+var email="My Email address is";
+alert(email + " tenison"+"256"+"@"+"gmail"+"."+"com");
+//part 7//
+var book="A smarter way to learn JavaScript";
+alert(" I am trying to learn from the Book " + book);
+// part 8 //
+document.write("Yah! I can write HTML content through JavaScript");
+//part 9//
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert("                     "+design);
